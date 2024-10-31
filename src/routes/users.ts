32b84fd4e55e4750501af52ffb765cb967e19f5f
Abraham Ziolo <abrahamziolo@gmail.com>
2024-10-31 12:58:35 -0400

    adding in User and Constituent crud routes

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,10 +1,72 @@
 import express, { Request, Response, NextFunction } from 'express';
+import { wrap } from '@mikro-orm/postgresql';
+
+import { em } from '../app.js';
+import { User } from '../models/user.js';
 
 const router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req: Request, res: Response, next: NextFunction) {
-  res.send('respond with a resource');
+/* GET all Users */
+router.get('/', async function(req: Request, res: Response, next: NextFunction) {
+  const users = await em.findAll(User);
+
+  res.send(users);
+});
+
+/* Create User */
+router.post('/', async function(req: Request, res: Response, next: NextFunction) {
+  try {
+    const user = new User();
+    user.firstName = req.body.firstName;
+    user.lastName = req.body.lastName;
+    user.email = req.body.email;
+    user.password = req.body.password;
+
+    em.persist(user);
+    await em.flush();
+  
+    res.json(user);
+  } catch (e: any) {
+    res.status(400).json({ message: e.message, stack: e.stack });
+  }
+});
+
+/* Update User */
+router.patch('/:id', async function(req: Request, res: Response, next: NextFunction) {
+  try {
+    const user = await em.findOne(User, parseInt(req.params.id));
+    
+    if (user === null) {
+      res.status(404).json({message: `user not found with id: ${req.params.id}`});
+      return;
+    } else {
+      wrap(user).assign(req.body);
+      await em.flush()
+
+      res.send(user);
+    }
+  } catch (e: any) {
+    res.status(400).json({ message: e.message, stack: e.stack });
+  }
+});
+
+/* GET User by id */
+router.get('/:id', async function(req: Request, res: Response, next: NextFunction) {
+  const id = parseInt(req.params.id);
+
+  if (Number.isNaN(id)) {
+    res.status(400).json({ message: 'please pass a valid id'})
+    return;
+  }
+  
+  const user = await em.findOne(User, parseInt(req.params.id));
+
+  if (user === null) {
+    res.status(404).json({message: `user not found with id: ${req.params.id}`});
+    return;
+  }
+
+  res.send(user);
 });
 
 export default router;

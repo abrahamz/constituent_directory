091fd3758801dd3d3c370337997aee698648dd40
Abraham Ziolo <abrahamziolo@gmail.com>
2024-10-31 12:17:33 -0400

    adding mikro-orm to handle db interactions

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,9 @@
 import express from 'express'
+import { MikroORM } from '@mikro-orm/postgresql';
 
 import indexRouter from './routes/index.js'
 import usersRouter from './routes/users.js'
+import config from './mikro-orm.config.js';
 
 const app = express();
 const port = 3000;
@@ -12,10 +14,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+// commenting out until db entities exist
+// connection can be tested using npx mikro-orm-esm debug
+// const orm = await MikroORM.init(config);
+
 app.listen(port, () => {
   // Log a message when the server is successfully running
   console.log(`Server is running on http://localhost:${port}`);
 });
-
-
-module.exports = app;

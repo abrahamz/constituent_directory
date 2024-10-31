80408ecbc408f9704f75fc5d304d0b8b86501aee
Abraham Ziolo <abrahamziolo@gmail.com>
2024-10-31 12:38:57 -0400

    adding first pass of models

---
+++ src/models/constituent.ts
@@ -0,0 +1,34 @@
+import { Entity, Property, Filter, Unique, ManyToOne } from '@mikro-orm/core';
+import { BaseEntity } from './baseModel.js';
+import { User } from './user.js';
+
+@Entity()
+export class Constituent extends BaseEntity {
+   @Property()
+   firstName!: string;
+
+   @Property()
+   lastName!: string;
+
+   @Property()
+   @Unique()
+   email!: string;
+
+   @Property()
+   street?: string;
+
+   @Property()
+   address2?: string;
+   
+   @Property()
+   city?: string;
+   
+   @Property()
+   state?: string;
+
+   @Property()
+   postalCode?: string;
+
+   @ManyToOne(() => User)
+   belongsTo!: User;
+}
